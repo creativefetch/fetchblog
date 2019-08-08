c7c1b19dc63671d7f62ab7aa5ddb048cd173a3ad
richard.vaughan <richard.vaughan@ocado.com>
2019-08-08 17:01:20 +0100

    loading page

--- src/components/LoadingPage/LoadingPage.js
+++ src/components/LoadingPage/LoadingPage.js
@@ -4,9 +4,9 @@ import Styles from "./LoadingPage.module.scss";
 const LoadingPage = () => {
     return(
         <div className={Styles.LoadingPage}>
-            <div>
+            <div className={Styles.LoadingContent}>
                 <h1>Welcome To The Fetch Blog</h1>
-                    <p>Loading...Please Wait...</p>
+                <p>Loading...Please Wait...</p>
             </div>
         </div>
     )
